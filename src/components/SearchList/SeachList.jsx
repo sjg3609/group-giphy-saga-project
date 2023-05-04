23ce5c1f79a3e0501725e51c1c2d6915cef31217
Sam Gossie <sjg3609@gmail.com>
2023-05-04 11:05:47 -0500

    Still working on the grid for the gifs to display

--- src/components/SearchList/SeachList.jsx
+++ src/components/SearchList/SeachList.jsx
@@ -1,19 +1,23 @@
 import SearchImage from "./SearchImage";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Grid, Container } from '@mui/material';
 
 function SearchList() {
     const gifList = useSelector(store => store.searchList);
 
-    return(
+    return (
         <div>
-            {
-                gifList.map((gif, i) => (
-                    <SearchImage key={i}
-                        gif = {gif}
-                    />
-                ))
-            }
+            <Container fixed >
+                {
+                    gifList.map((gif, i) => (
+                        <SearchImage key={i}
+                            gif={gif}
+                        />
+                    ))
+                }
+            </Container>
+
         </div>
     )
 }
--- src/components/SearchList/SearchImage.jsx
+++ src/components/SearchList/SearchImage.jsx
@@ -1,21 +1,38 @@
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { Box, Grid, Card, Paper } from '@mui/material';
+import { experimentalStyled as styled } from '@mui/material/styles';
 
-function SearchImage({gif}) {
+function SearchImage({ gif }) {
     const dispatch = useDispatch();
     const [status, setStatus] = useState(false);
 
     const toggleFavorite = (event) => {
         setStatus(!status);
-        if(status === true) {
-            const action = {type: 'SET_GIF_FAVORITE', payload: event.target.src};
+        if (status === true) {
+            const action = { type: 'SET_GIF_FAVORITE', payload: event.target.src };
             dispatch(action);
         }
     }
 
-    return(
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      }));
+
+    return (
         <>
-            <img onClick={toggleFavorite} src={gif.images.fixed_height.url} />
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={{ md: 3 }} >
+                   <Item md={3}>
+                        <img onClick={toggleFavorite} src={gif.images.fixed_height.url} />
+                    </Item> 
+            </Grid>
+        </Box>
+            
         </>
     );
 }
