77a3c48e2c5d6bd775cd6dbab608ce5fa352b069
Sam Gossie <sjg3609@gmail.com>
2023-05-04 09:51:32 -0500

    added put and delete routes to favorite.router

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -23,19 +23,36 @@ router.post('/', (req, res) => {
   }).catch(error => {
     console.log(`Error in POST /favorites: ${error}`);
     res.sendStatus(500);
-  })
+  });
   
 });
 
 // update given favorite with a category id
 router.put('/:favId', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+  const favoriteGif = req.body
+  console.log(req.body);
+  const queryText = `UPDATE favorites SET category_id = $1`
+  const queryValues = favoriteGif.category_id;
+  pool.query(queryText, queryValues).then(() => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log(`Error in completing Update in favorites query: ${error}`);
+    res.sendStatus(500);
+  })
+  
 });
 
 // delete a favorite
 router.delete('/', (req, res) => {
-  res.sendStatus(200);
+  const queryText = `DELETE from favorites WHERE id=$1`;
+  pool.query(queryText, [req.params.id]).then(() => {
+     res.sendStatus(200);
+  }).catch((error) => {
+    console.log(`Error in delete query for favorites: ${error}`);
+    res.sendStatus(500);
+  })
+ 
 });
 
 module.exports = router;
